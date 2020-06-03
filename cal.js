634f62b96d04a9bc4b12c820c37c95a7bda2849b
ERICHONG <f102041332@gmail.com>
2020-06-03 10:53:21 +0800

    轉成p

---
+++ cal.js
@@ -0,0 +1,13 @@
+
+// 計算函式
+function calculate(n){
+    return n * 100 + 'unit';
+}
+
+var text = 'Hi Webpack!';
+
+
+module.exports = {
+    name: 'Eric',
+    cal: calculate
+}
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // webpack -d 開發模式
 // webpack -v 開發模式
+
+// 呼叫
+var obj = require('./cal');
+
+
+console.log(obj.name);
+console.log(obj.cal(10));
 console.log('ok');
